57770bc20c4ba1c2571e8ed29d8c84ef7d9824eb
Penelope Y <penelopeysm@gmail.com>
2023-03-30 18:24:21 +0100

    Expand on example, add more docs

--- src/Reddit/Example.hs
+++ src/Reddit/Example.hs
@@ -1,19 +1,45 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import Control.Monad.Reader
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.Char8 as BL
-import Data.Text (Text)
+module Reddit.Example where
+
+import Control.Concurrent (threadDelay)
+import Control.Exception (SomeException, catch)
+import Control.Monad (when)
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Reddit
-import qualified Reddit.Auth as Auth
 import System.Environment (getEnv)
+import System.IO (stderr)
 
-getEnvAsText :: Text -> IO Text
+-- getEnv but acts on Text values
+getEnvAsText :: T.Text -> IO T.Text
 getEnvAsText = fmap T.pack . getEnv . T.unpack
 
+-- A callback function acting on comments, which takes an Int input state, the
+-- comment it sees, and does some kind of action on Reddit that ultimately
+-- returns another Int. The returned value will be used as the input the next
+-- time the callback is executed, i.e. on the next comment.
+replyToGreatHaskell :: Int -> Comment -> RedditT Int
+replyToGreatHaskell count cmt = do
+  -- Print details about the comment on standard output
+  let p = liftIO . T.putStrLn
+  p ""
+  p "Found new comment!"
+  p $ "By    : /u/" <> cmt.author
+  p $ "Link  : " <> cmt.url
+  p $ "Text  : " <> cmt.body
+  p $ T.pack ("Comments seen so far: " <> show count)
+
+  let triggerText = "Haskell is great!!!!!!!!"
+  let replyText = "Indeed, it is!"
+  -- Check whether it contains the trigger text, and reply if necessary
+  when
+    (triggerText `T.isInfixOf` cmt.body)
+    (p "Replying to it..." >> addNewComment cmt.id' replyText)
+  pure (count + 1)
 
 main :: IO ()
 main = do
@@ -23,27 +49,37 @@ main = do
   clientID <- getEnvAsText "REDDIT_ID"
   clientSecret <- getEnvAsText "REDDIT_SECRET"
   let userAgent = "your user-agent here"
-  let creds = Auth.Credentials {username = username,
-      password = password,
-      clientID = clientID,
-      clientSecret = clientSecret}
-  env <- Auth.withCredentials creds userAgent
+  -- The next line can be shortened with RecordWildCards if you want.
+  let creds =
+        Credentials
+          { username = username,
+            password = password,
+            clientID = clientID,
+            clientSecret = clientSecret
+          }
+  -- Get the RedditEnv term needed to run Reddit queries.
+  env <- withCredentials creds userAgent
 
-  let replyToGreatHaskell :: Int -> Comment -> RedditT Int
-      replyToGreatHaskell count cmt = do
-        let p = liftIO . T.putStrLn
-        p ""
-        p "Found new comment!"
-        p $ "By    : /u/" <> author cmt
-        p $ "Link  : " <> url cmt
-        p $ "Text  : " <> body cmt
-        p $ T.pack ("Comments seen so far: " <> show (count + 1))
-
-        let triggerText = "Haskell is great!"
-        let replyText = "Indeed, it is!"
-        when (triggerText `T.isInfixOf` (body cmt))
-         (p "Replying to it..." >> addNewComment (id' cmt) replyText)
-        pure (count + 1)
-
+  -- Run the bot.
   runRedditT' env $ do
-    stream True replyToGreatHaskell 0 (subredditComments "haskell")
+    commentStream True replyToGreatHaskell 1 "haskell"
+
+  -- commentStream is a recursive function and, in principle, should never end.
+  -- However, the two lines above this are quite naive: in practice you have to
+  -- account for exceptions, not least because Reddit servers seem to break
+  -- about once every day. The easiest way to do this is to use the combinators
+  -- in Control.Exception.
+  -- There is a separate issue, in that you can't extract the value of `count`
+  -- so far if an exception is raised. If you wanted to do this properly, you
+  -- probably need to use something like an IORef to store the value of count.
+  let protectedAction =
+        catch
+          -- The original action
+          (runRedditT' env $ commentStream True replyToGreatHaskell 1 "haskell")
+          -- If we get an exception, print it, wait 5 seconds, then try again.
+          ( \(e :: SomeException) -> do
+              T.hPutStrLn stderr ("Exception: " <> T.pack (show e))
+              threadDelay 5000000
+              protectedAction
+          )
+  protectedAction
