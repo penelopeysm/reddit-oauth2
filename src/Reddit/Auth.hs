52fceb0fb895124ed2d48d30e880a52af14c7d0d
Penelope Yong <penelopeysm@gmail.com>
2023-05-14 23:24:03 +0100

    Document scopes
    Closes #17

--- src/Reddit/Auth.hs
+++ src/Reddit/Auth.hs
@@ -39,37 +39,82 @@ data Credentials = Credentials
 
 -- * OAuth2 scopes
 
+--
+-- Scopes are used to limit the things which a token can do. You should ensure
+-- that you only request a token for the scopes that you need. When
+-- authenticating with Reddit, the user will be able to see which scopes are
+-- being requested.
+--
+-- To see which scope(s) are required by each endpoint, see
+-- https://www.reddit.com/dev/api/oauth. Note that this library implements only
+-- a small subset of endpoints, so most of these are not very useful right now.
+
+-- | All possible OAuth2 scopes. You can obtain this list via
+-- https://reddit.com/api/v1/scopes.
 data Scope
-  = ScopeCreddits
-  | ScopeModNote
-  | ScopeModContributors
-  | ScopeModMail
-  | ScopeModConfig
-  | ScopeSubscribe
-  | ScopeStructuredStyles
-  | ScopeVote
-  | ScopeWikiEdit
-  | ScopeMySubreddits
-  | ScopeSubmit
-  | ScopeModLog
-  | ScopeModPosts
-  | ScopeModFlair
-  | ScopeSave
-  | ScopeModOthers
-  | ScopeRead
-  | ScopePrivateMessages
-  | ScopeReport
-  | ScopeIdentity
-  | ScopeLiveManage
-  | ScopeAccount
-  | ScopeModTraffic
-  | ScopeWikiRead
-  | ScopeEdit
-  | ScopeModWiki
-  | ScopeModSelf
-  | ScopeHistory
-  | ScopeFlair
+  = -- | Spend the logged in user's Reddit gold.
+    ScopeCreddits
+  | -- | Access mod notes for subreddits moderated by the logged in user.
+    ScopeModNote
+  | -- | Add/remove users to/from approved user lists, ban/unban, and mute/unmute users on subreddits moderated by the logged in user.
+    ScopeModContributors
+  | -- | Access and manage modmail for subreddits moderated by the logged in user.
+    ScopeModMail
+  | -- | Manage the configuration, sidebar, and CSS for subreddits moderated by the logged in user.
+    ScopeModConfig
+  | -- | Manage subreddit subscriptions as well as \'friends\'.
+    ScopeSubscribe
+  | -- | Edit structured styles for subreddits moderated by the logged in user.
+    ScopeStructuredStyles
+  | -- | Vote on posts and comments.
+    ScopeVote
+  | -- | Edit wiki pages.
+    ScopeWikiEdit
+  | -- | Access the list of subreddits moderated, contributed to, and subscribed to.
+    ScopeMySubreddits
+  | -- | Submit links and comments.
+    ScopeSubmit
+  | -- | Access the moderation log in subreddits moderated by the logged in user.
+    ScopeModLog
+  | -- | Approve, remove, mark as (N)SFW, and distinguish content in subreddits moderated by the logged in user.
+    ScopeModPosts
+  | -- | Manage and assign flair in subreddits moderated by the logged in user.
+    ScopeModFlair
+  | -- | Save and unsave posts and comments.
+    ScopeSave
+  | -- | Invite and remove moderators on subreddits moderated by the logged in user.
+    ScopeModOthers
+  | -- | Access posts and comments.
+    ScopeRead
+  | -- | Read and send private messages. (Note: this does not include chat. There is, at present, no way to access chat via the public Reddit API.)
+    ScopePrivateMessages
+  | -- | Report, hide, and unhide content.
+    ScopeReport
+  | -- | Get Reddit username and signup date.
+    ScopeIdentity
+  | -- | Manage settings and contributors of live threads.
+    ScopeLiveManage
+  | -- | Update preferences and other account information. Does not grant access to email or password.
+    ScopeAccount
+  | -- | Access traffic stats in subreddits moderated by the user.
+    ScopeModTraffic
+  | -- | Read wiki pages.
+    ScopeWikiRead
+  | -- | Edit and delete posts and comments.
+    ScopeEdit
+  | -- | Change editors and visibility of wiki pages in subreddits moderated by the logged in user.
+    ScopeModWiki
+  | -- | Accept invitations to moderate a subreddit and step down from moderating subreddits.
+    ScopeModSelf
+  | -- | Access voting history, saved submissions, and hidden submissions.
+    ScopeHistory
+  | -- | Edit subreddit and post flair.
+    ScopeFlair
   deriving (Eq, Ord, Enum, Bounded, Show)
+
+-- | A quick way to get all scopes.
+allScopes :: S.Set Scope
+allScopes = S.fromList [minBound .. maxBound]
 
 parseScopes :: Text -> S.Set Scope
 parseScopes t = case T.split (\c -> c == ',' || c == ' ') t of
@@ -141,9 +186,6 @@ showScopes = T.intercalate "," . map showOneScope . S.toList
       ScopeVote -> "vote"
       ScopeWikiEdit -> "wikiedit"
       ScopeWikiRead -> "wikiread"
-
-allScopes :: S.Set Scope
-allScopes = S.fromList [minBound .. maxBound]
 
 -- * Access tokens
 
